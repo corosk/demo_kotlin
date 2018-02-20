a93f516fe0ecd0de0ee581f34d0d0a6cf3cc4559
corosk <corosk>
2018-02-20 23:08:56 +0900

    tripleURL追加

--- src/main/kotlin/com/example/demo/controller/MyController.kt
+++ src/main/kotlin/com/example/demo/controller/MyController.kt
@@ -7,5 +7,12 @@ import org.springframework.web.bind.annotation.*
 class MyController {
     @ApiOperation(value = "doc header...", notes = "detailed doc...")
     @RequestMapping(value = "/double", method = arrayOf(RequestMethod.GET))
-    fun doubleValue(number: Int) = 2 * number
+    fun doubleValue(number: Int): Int {
+        return 2 * number
+    }
+
+    @RequestMapping(value = "/triple", method = arrayOf(RequestMethod.GET))
+    fun tripleValue(number: Int): Int {
+        return 3 * number
+    }
 }
